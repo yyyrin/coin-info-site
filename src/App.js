a8474b49ef87de310221641f591a7cb90a419195
yeguu <qsc130@gmail.com>
2023-12-08 23:47:34 +0900

    #2.7_Themes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
@@ -6,46 +6,17 @@ const Wrapper = styled.div`
   width: 100vw;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji}:hover {
-    font-size: 98px;
-  }
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">😊</Emoji>
-      </Box>
-      <Emoji as="p">❤️</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { ThemeProvider } from "styled-components";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+const lightTheme = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
 
 root.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );

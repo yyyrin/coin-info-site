ebc2bfe2b6463a3689f9366e85dbdbebefa603d0
yeguu <qsc130@gmail.com>
2024-01-10 19:29:25 +0900

    #3.2_Typing_the_Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import styled from "styled-components";
+import Circle from "./Circle";
 
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-function App() {
+const App = () => {
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
-}
+};
 
 export default App;

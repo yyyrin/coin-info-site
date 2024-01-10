ab147142aaee7fd55aae3f17c59b36916724dbed
yeguu <qsc130@gmail.com>
2024-01-11 00:50:58 +0900

    #3.3_Optional_Props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,18 +1,34 @@
 import styled from "styled-components";
 
-interface CircleProps {
+interface ContainerProps {
   bgColor: string;
+  borderColor: string; // required
 }
 
-const Container = styled.div<CircleProps>`
+const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
-const Circle = ({ bgColor }: CircleProps) => {
-  return <Container bgColor={bgColor} />;
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string; // optional props
+  text?: string;
+}
+
+const Circle = ({
+  bgColor,
+  borderColor,
+  text = "default text", // default 값 설정
+}: CircleProps) => {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
 };
 
 export default Circle;

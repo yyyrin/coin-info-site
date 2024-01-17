f36ea65fdb7120f29f9e26cd5356b75567f79093
yeguu <qsc130@gmail.com>
2024-01-17 18:01:56 +0900

    feat: candlestick chart 구현

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -3,6 +3,12 @@ import { getCoinHistory } from "../api";
 import ReactApexChart from "react-apexcharts";
 import { useRecoilValue } from "recoil";
 import { isDarkAtom } from "../atoms";
+import styled from "styled-components";
+
+const ChartContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 interface IHistorical {
   time_open: number;
@@ -28,64 +34,108 @@ const Chart = ({ coinId }: ChartProps) => {
     ["ohlcv", coinId],
     () => getCoinHistory(coinId),
     {
-      refetchInterval: 10000, // 해당 query를 10초마다 refetch
+      // refetchInterval: 10000, // 해당 query를 10초마다 refetch
     }
   );
+
+  console.log(data);
 
   return (
     <div>
       {isLoading ? (
         "Loading chart..."
       ) : (
-        <ReactApexChart
-          type="line" // 차트 유형
-          // 차트에 표시하려는 데이터
-          series={[
-            {
-              name: "price",
-              data: data?.map((price) => Number(price.close)) ?? [],
-            },
-          ]}
-          // 차트의 구성 옵션
-          options={{
-            theme: { mode: isDark ? "dark" : "light" },
-            chart: {
-              height: 300, // 차트의 높이
-              width: 500, // 차트의 너비
-              toolbar: {
+        <ChartContainer>
+          <ReactApexChart
+            type="line" // 차트 유형
+            // 차트에 표시하려는 데이터
+            series={[
+              {
+                name: "price",
+                data: data?.map((price) => Number(price.close)) ?? [],
+              },
+            ]}
+            // 차트의 구성 옵션
+            options={{
+              theme: { mode: isDark ? "dark" : "light" },
+              chart: {
+                height: 300, // 차트의 높이
+                width: 500, // 차트의 너비
+                toolbar: {
+                  show: false,
+                },
+                background: "transparent",
+              },
+              grid: {
                 show: false,
               },
-              background: "transparent",
-            },
-            grid: {
-              show: false,
-            },
-            stroke: {
-              curve: "smooth",
-              width: 4,
-            },
-            xaxis: {
-              labels: { show: false },
-              axisTicks: { show: false },
-              axisBorder: { show: false },
-              type: "datetime",
-              categories: data?.map((price) =>
-                new Date(price.time_close * 1000).toUTCString()
-              ),
-            },
-            yaxis: {
-              show: false,
-            },
-            fill: {
-              type: "gradient",
-              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
-            },
-            colors: ["#0fbcf9"],
-            tooltip: {
-              y: { formatter: (value) => `$ ${value.toFixed(2)}` },
-            },
-          }}
-        />
+              stroke: {
+                curve: "smooth",
+                width: 4,
+              },
+              xaxis: {
+                labels: { show: false },
+                axisTicks: { show: false },
+                axisBorder: { show: false },
+                type: "datetime",
+                categories: data?.map((price) =>
+                  new Date(price.time_close * 1000).toUTCString()
+                ),
+              },
+              yaxis: {
+                show: false,
+              },
+              fill: {
+                type: "gradient",
+                gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+              },
+              colors: ["#0fbcf9"],
+              tooltip: {
+                y: { formatter: (value) => `$ ${value.toFixed(2)}` },
+              },
+            }}
+          />
+          <ReactApexChart
+            type="candlestick"
+            height={300}
+            series={[
+              {
+                data:
+                  data?.map((price) => ({
+                    x: new Date(price.time_close * 1000).toUTCString(),
+                    y: [
+                      Number(price.open),
+                      Number(price.high),
+                      Number(price.low),
+                      Number(price.close),
+                    ],
+                  })) ?? [],
+              },
+            ]}
+            options={{
+              theme: { mode: isDark ? "dark" : "light" },
+              chart: {
+                width: 500,
+                toolbar: {
+                  show: false,
+                },
+                background: "transparent",
+              },
+              xaxis: {
+                labels: { show: false },
+                axisTicks: { show: false },
+                axisBorder: { show: false },
+                type: "datetime",
+              },
+              yaxis: {
+                show: false,
+              },
+              grid: {
+                show: false,
+              },
+            }}
+          />
+        </ChartContainer>
       )}
     </div>
   );
